712717eb40e1b6e4c933b70cb53cb39e88a091f5
fajrCode <fajri42vandi@gmail.com>
2023-12-09 21:07:52 +0700

    update

--- app.py
+++ app.py
@@ -88,4 +88,6 @@ def prediction():
 
 #running like void
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True,
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 8080)))
